b6e8108bb79b9a6b77241910cb14cf67bbde1e00
realestindigo <jLoupias@roc-dev.com>
2019-05-07 10:21:32 +0100

    'done'

---
+++ javascript.js
@@ -0,0 +1,9 @@
+document.getElementById("img1").src="img/" + (Math.floor(Math.random() * 9) +1 ) + ".jpg";
+document.getElementById("img2").src="img/" + (Math.floor(Math.random() * 9) +1 ) + ".jpg";
+document.getElementById("img3").src="img/" + (Math.floor(Math.random() * 9) +1 ) + ".jpg";
+document.getElementById("img4").src="img/" + (Math.floor(Math.random() * 9) +1 ) + ".jpg";
+document.getElementById("img5").src="img/" + (Math.floor(Math.random() * 9) +1 ) + ".jpg";
+document.getElementById("img6").src="img/" + (Math.floor(Math.random() * 9) +1 ) + ".jpg";
+document.getElementById("img7").src="img/" + (Math.floor(Math.random() * 9) +1 ) + ".jpg";
+document.getElementById("img8").src="img/" + (Math.floor(Math.random() * 9) +1 ) + ".jpg";
+document.getElementById("img9").src="img/" + (Math.floor(Math.random() * 9) +1 ) + ".jpg";
